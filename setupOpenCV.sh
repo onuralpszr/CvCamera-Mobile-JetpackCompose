1cc83d039565999e02e2f7872542475b9d74f11c
Onuralp SEZER <thunderbirdtr@fedoraproject.org>
2023-01-18 03:13:07 +0300

    feat: 🚀 opencv 4.7.0 added
    Signed-off-by: Onuralp SEZER <thunderbirdtr@fedoraproject.org>

---
+++ setupOpenCV.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+wget -O opencv-4.7.0-android-sdk.zip https://github.com/opencv/opencv/releases/download/4.7.0/opencv-4.7.0-android-sdk.zip
+unzip -qq opencv-4.7.0-android-sdk.zip
+mv OpenCV-android-sdk opencvsdk470
+sudo sed -i 's/compileSdkVersion 26/compileSdkVersion 33/g' opencvsdk470/sdk/build.gradle
+sudo sed -i 's/targetSdkVersion 26/targetSdkVersion 33/g' opencvsdk470/sdk/build.gradle
